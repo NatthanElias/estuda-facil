71be2ebc6733165261331ff3c34675426e987786
Natthan <natthandosantos@gmail.com>
2024-07-20 16:11:45 -0300

    refactor: Atualiza BD

--- estudafacil-bd.sql
+++ estudafacil-bd.sql
@@ -1,23 +1,31 @@
-create table categorias (
-	codigo serial not null primary key, 
-	nome varchar (40) not null
+CREATE TABLE decks (
+    codigo SERIAL PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    descricao TEXT,
+    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 );
 
-create table cards (
-	codigo serial not null primary key,
-	id_categoria INT NOT NULL,
+CREATE TABLE cards (
+    codigo SERIAL PRIMARY KEY,
     pergunta TEXT NOT NULL,
     resposta TEXT NOT NULL,
-    FOREIGN KEY (id_categoria) REFERENCES categorias(codigo)
-)
+    deck_id INTEGER NOT NULL,
+    FOREIGN KEY (deck_id) REFERENCES decks (codigo)
+);
 
-insert into categorias (nome) values ('Teoria da Computação') , ('Redes') , ('Páginas Web');
 
-select * from categorias
+INSERT INTO decks (nome, descricao)
+VALUES
+    ('Redes de Computadores', 'Deck sobre Redes de Computadores'),
+    ('Teoria da Computação', 'Deck sobre Teoria da Computação'),
+    ('Páginas Web', 'Deck sobre Páginas Web');
+	
+INSERT INTO cards (pergunta, resposta, deck_id)
+VALUES
+    ('O que é DNS?', 'DNS (Domain Name System) é o sistema responsável por traduzir nomes de domínio em endereços IP, permitindo a localização de computadores e serviços na rede.', 1),
+    ('Qual diferença dos autômatos finitos determinísticos e não-determinísticos?', 'Autômatos finitos determinísticos (DFA) têm uma única transição possível para cada símbolo de entrada em cada estado, enquanto autômatos finitos não-determinísticos (NFA) podem ter várias transições possíveis para o mesmo símbolo.', 2),
+    ('O que é uma PWA?', 'Uma PWA (Progressive Web App) é uma aplicação web que utiliza as mais recentes tecnologias web para fornecer uma experiência de usuário similar à de aplicativos nativos.', 3);
+	
+select * from decks
 
-INSERT INTO cards (id_categoria, pergunta, resposta) VALUES
-(1, 'O que é um automato finito nao deterministico?', 'Um modelo de máquina teórica que, para cada par de estado e símbolo, a máquina pode fazer uma transição para zero ou mais estados'),
-(2, 'Qual função de um servidor DNS?', 'Resolver nomes de domínio em endereços IP e vice-versa'),
-(3, 'O que é um PWA?', 'Uma aplicação web que oferece funcionalidades adicionais como funcionamento offline, notificações push e atualização em segundo plano, proporcionando uma experiência semelhante à de um aplicativo nativo');
-
-SELECT * FROM cards
+select * from cards
