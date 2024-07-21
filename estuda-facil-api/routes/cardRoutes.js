3bd5cc875a3057bfa2ea6acc2324babb4c6722b6
Natthan <natthandosantos@gmail.com>
2024-07-21 10:01:32 -0300

    fix: Corrigido nome ao exportar a rota cards

--- estuda-facil-api/routes/cardRoutes.js
+++ estuda-facil-api/routes/cardRoutes.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
+
 const { getCards } = require('../controllers/cardController');
 
 const cardRoutes = new Router();
 
 cardRoutes.get('/cards', getCards);
 
-module.exports = routerCards;
+module.exports = { cardRoutes };
